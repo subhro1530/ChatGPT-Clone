4e3ebf12603b9a31da9881381301c40d7d8eaa5d
subhro1530 <shaswata.ssaha@gmail.com>
2023-08-19 16:34:03 +0530

    changes saved 11

--- public/script.js
+++ public/script.js
@@ -85,17 +85,78 @@ function sendMessage() {
       chatHistoryData.push({ user: false, message: assistantReply });
       // Store updated chat history data in local storage
       localStorage.setItem("chatHistory", JSON.stringify(chatHistoryData));
+      if (!regenerateButtonGenerated) {
+        const regenerateButton = document.createElement("button");
+        regenerateButton.textContent = "Regenerate";
+        regenerateButton.classList.add("regenerate-button");
+        regenerateButton.addEventListener("click", () => {
+          const lastUserMessage = getLastUserMessage();
+          if (lastUserMessage) {
+            // Display "Typing..." immediately
+            appendMessage("bot", "Typing...");
+
+            // Resend the last user message
+            sendMessage(lastUserMessage);
+          }
+        });
+
+        // Append the regenerate button to the messageDiv
+        messageDiv.appendChild(regenerateButton);
+
+        regenerateButtonGenerated = true; // Set to true after generating the button
+      }
     })
     .catch((error) => {
       console.error("Error:", error);
     });
 }
 
+function getLastUserMessage() {
+  for (let i = chatHistoryData.length - 1; i >= 0; i--) {
+    if (chatHistoryData[i].user) {
+      return chatHistoryData[i].message;
+    }
+  }
+  return null;
+}
+userInput.addEventListener("input", () => {
+  const userMessage = userInput.value.trim();
+
+  if (userMessage === "") {
+    sendButton.style.backgroundColor = "transparent";
+    sendButton.style.color = "grey";
+  } else {
+    sendButton.style.backgroundColor = "#19c37d";
+    sendButton.style.color = "white";
+  }
+});
 
 function appendMessage(role, message) {
   const messageDiv = document.createElement("div");
   messageDiv.classList.add(role);
-  messageDiv.textContent = message;
+
+  const messageContent = document.createElement("div");
+  messageContent.textContent = message;
+
+  if (role === "user") {
+    const userImage = document.createElement("img");
+    userImage.src = "images/user-icon.png"; // Replace with the path to your user icon image
+    userImage.alt = "User Icon"; // Alt text for accessibility
+    userImage.classList.add("user-image");
+
+    messageDiv.appendChild(userImage);
+  }
+
+  if (role === "bot") {
+    const botImage = document.createElement("img");
+    botImage.src = "images/chatgpt-logo.png"; // Replace with the correct path to your image
+    botImage.alt = "ChatGPT Logo"; // Alt text for accessibility
+    botImage.classList.add("bot-image");
+
+    messageDiv.appendChild(botImage);
+  }
+
+  messageDiv.appendChild(messageContent);
 
   if (role === "bot") {
     const copyButton = document.createElement("button");
@@ -109,17 +170,41 @@ function appendMessage(role, message) {
     messageDiv.appendChild(copyButton);
   }
 
+  // Add a "Regenerate" button for bot responses
+  if (role === "bot") {
+    const regenerateButton = document.createElement("button");
+    regenerateButton.textContent = "Regenerate";
+    regenerateButton.classList.add("regenerate-button");
+    regenerateButton.addEventListener("click", () => {
+      const lastUserMessage = getLastUserMessage();
+      if (lastUserMessage) {
+        // Display "Typing..." immediately
+        appendMessage("bot", "Typing...");
+
+        // Resend the last user message
+        sendMessage(lastUserMessage);
+      }
+    });
+
+    // Append the regenerate button to the messageDiv
+    messageDiv.appendChild(regenerateButton);
+  }
+
   chatBox.appendChild(messageDiv);
+
+  // Update scroll position to keep the chat-box scrolled to the bottom
   chatBox.scrollTop = chatBox.scrollHeight;
 
   // Store updated chat history data in local storage
   localStorage.setItem("chatHistory", JSON.stringify(chatHistoryData));
-}
 
+  // Update scroll position to keep the chat-box scrolled to the bottom
+  scrollToBottom();
+}
 
 function copyToClipboard(text) {
   const textArea = document.createElement("textarea");
-  textArea.value = text;
+  textArea.value = text; 
   document.body.appendChild(textArea);
   textArea.select();
   document.execCommand("copy");
@@ -168,10 +253,30 @@ function appendChatHistory() {
     messageDiv.classList.add(chat.user ? "user" : "bot");
     messageDiv.textContent = chat.message;
     chatHistory.appendChild(messageDiv);
+    // appendChatEntry(role, chat.message);
   }
 
   chatBox.scrollTop = chatBox.scrollHeight; // Scroll to the bottom of the chat history
 }
+
+function appendChatEntry(role, message) {
+  const entryDiv = document.createElement("div");
+  entryDiv.classList.add(role);
+
+  const messageIcon = document.createElement("img");
+  messageIcon.src = "https://icons.veryicon.com/png/o/miscellaneous/ios-icon-library/message-message-3.png"; // Replace with the path to your message icon image
+  messageIcon.alt = "Message Icon"; // Alt text for accessibility
+  messageIcon.classList.add("message-icon");
+
+  const messageContent = document.createElement("div");
+  messageContent.textContent = message;
+
+  entryDiv.appendChild(messageIcon);
+  entryDiv.appendChild(messageContent);
+
+  chatHistory.appendChild(entryDiv);
+}
+
 
 function clearAllChatHistory() {
   chatHistoryData = []; // Clear all chat history data
@@ -179,6 +284,15 @@ function clearAllChatHistory() {
   // Update chat history display and local storage
   appendChatHistory();
   updateLocalStorage();
+
+  chatHistoryData = []; // Clear all chat history data
+
+  // Update chat history display and local storage
+  appendChatHistory();
+  updateLocalStorage();
+
+  // Scroll to the bottom after clearing history
+  scrollToBottom();
 }
 function clearChatHistoryItem(index) {
   chatHistoryData.splice(index, 1); // Remove the specified item from chat history data
@@ -237,8 +351,26 @@ userInput.addEventListener("keydown", (event) => {
     sendButton.click(); // Programmatically trigger a click on the send button
   }
 });
-var elem = document.querySelector(".chat-box");
-elem.setInterval(function () {
-  
-  elem.scrollTop = elem.scrollHeight;
-}, 5000);
+
+// Function to scroll the chat-box to the bottom
+function scrollToBottom() {
+  chatBox.scrollTop = chatBox.scrollHeight;
+}
+
+// Create a function that simulates the typewriter animation:
+function typeWriterEffect(message, callback) {
+  let i = 0;
+  const typingInterval = 50; // Adjust this interval for typing speed
+
+  const typingIntervalId = setInterval(() => {
+    if (i < message.length) {
+      appendMessage("bot", message.substring(0, i + 1));
+      i++;
+    } else {
+      clearInterval(typingIntervalId);
+      if (typeof callback === "function") {
+        callback();
+      }
+    }
+  }, typingInterval);
+}
